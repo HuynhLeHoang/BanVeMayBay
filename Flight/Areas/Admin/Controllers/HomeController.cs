a30d3ba95f2396c3f0ed077af9238a106e105b5b
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-10-28 23:00:30 +0700

    Huynh

--- Flight/Areas/Admin/Controllers/HomeController.cs
+++ Flight/Areas/Admin/Controllers/HomeController.cs
@@ -56,20 +56,10 @@ namespace Flight.Areas.Admin.Controllers
                 
                 return View("Success", TempData["notice"] = "Thêm chuyến bay thành công!");
             }
-            catch (DbEntityValidationException e)
+            catch 
             {
-                foreach (var eve in e.EntityValidationErrors)
-                {
-                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
-                    foreach (var ve in eve.ValidationErrors)
-                    {
-                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                            ve.PropertyName, ve.ErrorMessage);
-                    }
-                }
-                throw;
-                //return View("Success", TempData["notice"] = "Đã có lỗi xảy ra!");
+                
+                return View("Success", TempData["notice"] = "Đã có lỗi xảy ra!");
             }
         }
 
