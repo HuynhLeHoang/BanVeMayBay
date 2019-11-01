4f38694c79de89adc0c3f6e1282be56cd8b8b652
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-11-01 09:13:41 +0700

    Huynh

--- Flight/Areas/Admin/Controllers/BaseController.cs
+++ Flight/Areas/Admin/Controllers/BaseController.cs
@@ -11,8 +11,13 @@ namespace Flight.Areas.Admin.Controllers
     {
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var session = (UserLogin)Session[CommonSession.USER_SESSION];
-            if(session == null)
+            var session = (UserLogin)Session[CommonSession.AMDIN_SESSION];
+            var session1 = (UserLogin)Session[CommonSession.USER_SESSION];
+            if (session1 != null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { controller = "Home", action = "Index", Area = "~" }));
+            }
+            else if (session == null)
             {
                 filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { controller = "Login", action = "Login", Area = "Admin" }));
             }
