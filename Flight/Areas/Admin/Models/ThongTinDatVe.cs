51d4e14f033257e36619c74f56a1635cfc5df9bf
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-12-16 09:56:33 +0700

    Huynh

---
+++ Flight/Areas/Admin/Models/ThongTinDatVe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Flight.Models.Entity;
+
+namespace Flight.Areas.Admin.Models
+{
+    public class ThongTinDatVe
+    {
+        private string MaVe;
+        private string HoTen;
+        private string KhuVuc;
+        private string SDT;
+        private string Email;
+        private string DiaChi;
+        private IList<adult> adults;
+        private IList<child> childs;
+        private IList<infant> infants;
+    }
+}
